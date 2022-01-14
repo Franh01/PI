23f06eeb56077f303815a9e36f52e9f3c018dbf6
Franh01 <fran_lopez9@live.com>
2022-01-14 18:08:32 -0300

    deploy

--- api/src/app.js
+++ api/src/app.js
@@ -11,6 +11,7 @@ const server = express();
 
 server.name = 'API';
 
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
@@ -22,7 +23,6 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
-server.use(cors());
 
 server.use('/', routes);
 
